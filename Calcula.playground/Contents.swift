231fdbaeba262f919a5e77ce015186993fabe873
Thiago <thiagovalentim@hotmail.com.br>
2022-09-30 14:40:59 -0300

    Challenge resolution
    Foi utilizado o principio de responsabilidade única para a class e métodos.

--- Calcula.playground/Contents.swift
+++ Calcula.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// Thiago Valentin 30/09/2022
+
 enum TypeOperacao {
     case adicao
     case subtracao
@@ -10,9 +12,39 @@ enum TypeOperacao {
 
 func calcula(_ valor1: Int, _ valor2: Int, _ operacao: TypeOperacao) -> Int {
     
-    // este return é só pra nao ficar dando erro.
-    return 0
+    switch operacao {
+        
+    case .adicao:
+        return adiction(valor1: valor1, valor2: valor2)
+    case .subtracao:
+        return subtraction(valor1: valor1, valor2: valor2)
+    case .multiplicacao:
+        return multiplication(valor1: valor1, valor2: valor2)
+    case .divisao:
+        return division(valor1: valor1, valor2: valor2)
+    default :
+        return 0
+    }
 }
+
+func adiction(valor1: Int, valor2: Int) -> Int{
+    return valor1 + valor2
+}
+
+func subtraction(valor1: Int, valor2: Int) -> Int{
+    return valor1 - valor2
+}
+
+func multiplication(valor1: Int, valor2: Int) -> Int{
+    return valor1 * valor2
+}
+
+func division(valor1: Int, valor2: Int) -> Int{
+    return valor1 / valor2
+}
+
+
+
 
 print("Resultado: \(calcula(2,2,.adicao))")
 print("Resultado: \(calcula(2,2,.subtracao))")
