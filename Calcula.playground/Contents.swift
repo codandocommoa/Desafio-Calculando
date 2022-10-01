bd607ad71a9859f7832fc95b93d07b77d7e2795c
Jhonatanbss <jhonatanbruno7@hotmail.com>
2022-09-30 23:05:02 -0300

    desafio1

--- Calcula.playground/Contents.swift
+++ Calcula.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/*
 enum TypeOperacao {
     case adicao
     case subtracao
@@ -18,3 +19,22 @@ print("Resultado: \(calcula(2,2,.adicao))")
 print("Resultado: \(calcula(2,2,.subtracao))")
 print("Resultado: \(calcula(2,2,.multiplicacao))")
 print("Resultado: \(calcula(2,2,.divisao))")
+*/
+ 
+func adicao(_ valor1: Int, _ valor2: Int) -> Int {
+    return valor1 + valor2
+}
+func subtracao(_ valor1: Int, _ valor2: Int) -> Int {
+    return valor1 - valor2
+}
+func multiplicacao(_ valor1: Int, _ valor2: Int) -> Int {
+    return valor1 * valor2
+}
+func divisao(_ valor1: Int, _ valor2: Int) -> Int {
+    return valor1 / valor2
+}
+
+print("Resultado: \(adicao(2,2))")
+print("Resultado: \(subtracao(2,2))")
+print("Resultado: \(multiplicacao(2,2))")
+print("Resultado: \(divisao(2,2))")
