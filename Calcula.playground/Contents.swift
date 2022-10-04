6acdc03370cb0c08f2475093622d056a3e1c26cd
Dante Gabriel Tokomoto Takatu <113074962+Dante-Gabriel-Tokomoto-Takatu@users.noreply.github.com>
2022-10-03 22:35:13 -0300

    Estudos iOS
    Salvee professor! Estou na BackFront+ e te conheci por lá no canal do yt, sobre GitHub estou aprendendo com Gustavo Guanabara no yt, pega leve comigo estou começando haha! Estou ansioso para as aulas de arquitetura, abraços!

--- Calcula.playground/Contents.swift
+++ Calcula.playground/Contents.swift
@@ -8,13 +8,34 @@ enum TypeOperacao {
 }
 
 
-func calcula(_ valor1: Int, _ valor2: Int, _ operacao: TypeOperacao) -> Int {
+func calcula(valor1: Int, valor2: Int, operacao: TypeOperacao) -> Int {
     
     // este return é só pra nao ficar dando erro.
     return 0
+    
+func soma(valor1: Int, valor2: Int) -> Int {
+    return valor1 + valor2
+}
+func subtracao(valor1: Int, valor2: Int) -> Int {
+    return valor1 - valor2
+}
+func multiplicacao(valor1: Int, valor2: Int) -> Int {
+    return valor1 * valor2
+}
+func divisao(valor1: Int, valor2: Int) -> Int {
+    return valor1 / valor2
 }
 
-print("Resultado: \(calcula(2,2,.adicao))")
-print("Resultado: \(calcula(2,2,.subtracao))")
-print("Resultado: \(calcula(2,2,.multiplicacao))")
-print("Resultado: \(calcula(2,2,.divisao))")
+//soma("Resultado: \(calcula(2,2,.adicao))")
+    soma(2, com: 2)
+    
+//print2("Resultado: \(calcula(2,2,.subtracao))")
+    subtacao(2, com: 2)
+    
+//print3("Resultado: \(calcula(2,2,.multiplicacao))")
+    multiplicacao(2, com: 2)
+    
+//print4("Resultado: \(calcula(2,2,.divisao))")
+    divisao(2, com: 2)
+    
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
