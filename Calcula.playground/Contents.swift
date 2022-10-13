029d4ba73a05c7613936aec9e82db181ef790fa8
Wallace Santos <wallace_bcc@hotmail.com>
2022-10-12 22:38:01 -0300

    Desafio do moa, obrigado pelos conteúdos

--- Calcula.playground/Contents.swift
+++ Calcula.playground/Contents.swift
@@ -9,9 +9,12 @@ enum TypeOperacao {
 
 
 func calcula(_ valor1: Int, _ valor2: Int, _ operacao: TypeOperacao) -> Int {
-    
-    // este return é só pra nao ficar dando erro.
-    return 0
+    switch operacao{
+        case .adicao: return valor1 + valor2
+        case .subtracao: return valor1 - valor2
+        case .multiplicacao: return valor1 * valor2
+        case .divisao: return valor1 / valor2
+    }
 }
 
 print("Resultado: \(calcula(2,2,.adicao))")
