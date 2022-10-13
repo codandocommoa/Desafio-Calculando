0c2128c7787a1dd0e977a37991f9c791c1a2344c
Wallace Santos <wallace_bcc@hotmail.com>
2022-10-12 23:01:19 -0300

    desafio moa

--- Calcula.playground/Contents.swift
+++ Calcula.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+
+
 enum TypeOperacao {
     case adicao
     case subtracao
@@ -7,13 +9,27 @@ enum TypeOperacao {
     case divisao
 }
 
+func adicao(_ valor1: Int, _ valor2: Int) -> Int{
+    return valor1 + valor2
+}
+func subtracao(_ valor1: Int, _ valor2: Int) -> Int{
+    return valor1 - valor2
+}
+func multiplicacao(_ valor1: Int, _ valor2: Int) -> Int{
+    return valor1 * valor2
+}
+func divisao(_ valor1: Int, _ valor2: Int) -> Int{
+    return valor1 / valor2
+}
+
+
 
 func calcula(_ valor1: Int, _ valor2: Int, _ operacao: TypeOperacao) -> Int {
     switch operacao{
-        case .adicao: return valor1 + valor2
-        case .subtracao: return valor1 - valor2
-        case .multiplicacao: return valor1 * valor2
-        case .divisao: return valor1 / valor2
+        case .adicao: return adicao(valor1, valor2)
+        case .subtracao: return subtracao(valor1, valor2)
+        case .multiplicacao: return multiplicacao(valor1, valor2)
+        case .divisao: return divisao(valor1, valor2)
     }
 }
 
